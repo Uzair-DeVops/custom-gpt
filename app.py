d861d5554d91a1f6ed5f69f6e0d75222596bcb49
Muhammad Uzair <uzairyasin83@gmail.com>
2025-01-26 16:51:56 +0500

    c

--- app.py
+++ app.py
@@ -9,11 +9,11 @@ Userquery = st.text_input("write which you want to translate ")
 prompt =f"""
 You have to work as a translator. Your task is to convert the given user query into five languages:
 
-1. **Urdu**  
-2. **French**  
-3. **German**  
-4. **Chinese**  
-5. **Japanese**  
+1. Urdu
+2. French
+3. German  
+4. Chinese  
+5. Japanese  
 
 **User Query:** {Userquery}
 
